93008df2bd0fc81be8b2fa851a45857172d2c446
Karim Aboelela <karim.ali.dev@gmail.com>
2021-12-19 14:57:22 +0100

    Update header logout link

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   const renderLogoutLink = () => {
-    const token = sessionStorage.getItem("__tok");
-    if (token) {
+    /**
+     * It would be better to depend on the token here instead as other places
+     * That would require to centralized the state in a reactive way instead of checking the sessionstorage
+     * To be discussed
+     */
+    if (location.pathname === "/") {
       return (
         <Link onClick={() => logout()} to="/login" data-testid="logout">
           <span className="inline-block text-sm px-4 py-2 leading-none border rounded text-teal-200 border-white hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0">
